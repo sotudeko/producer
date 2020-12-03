77ba0af31012a9197b8796afaf66604debbaa164
sotudeko <sotudeko@sonatype.com>
2020-12-03 18:39:01 +0000

    1.2

--- src/main/java/org/demo/Producer.java
+++ src/main/java/org/demo/Producer.java
@@ -12,30 +12,4 @@ public class Producer {
     return "Message from the Producer... Hello World!";
   }
 
-  public StringBuilder getData(String iqApiEndpoint) throws IOException {
-
-    StringBuilder content = new StringBuilder();
-
-    URL url = new URL(iqApiEndpoint);
-
-    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-    conn.setRequestMethod("GET");
-    conn.setRequestProperty("Accept", "application/json");
-    conn.setRequestProperty("Authorization", "Basic YWRtaW46YWRtaW4xMjM=");
-
-    if (conn.getResponseCode() != 200) {
-      throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
-    }
-
-    BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-    String line;
-
-    while ((line = br.readLine()) != null) {
-      content.append(line + "\n");
-    }
-
-    return content;
-  }
-
 }
