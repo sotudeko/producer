6bd613c9874f6b214972751fa8ab27325db6b227
sotudeko <sotudeko@sonatype.com>
2020-12-03 18:39:16 +0000

    1.2

--- src/main/java/org/demo/Producer.java
+++ src/main/java/org/demo/Producer.java
@@ -1,11 +1,5 @@
 package org.demo;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 public class Producer {
 
   public String hello(){
